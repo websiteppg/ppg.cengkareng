7dc2bb647f167b4d9eda4ce0a5dc1924dba99a1a
websiteppg <charbeginner@gmail.com>
2025-09-22 14:42:43 +0700

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['localhost', 'foozwfyfgqtgofffnqbq.supabase.co'],
-  },
-  env: {
-    TIMEZONE: 'Asia/Jakarta',
-    MAX_PARTICIPANTS: '100',
-  },
-  experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js']
-  }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
